e3a4792a7549626dac00d325f0facdc24acc6724
Mihir Shri <mihir.shri10@gmail.com>
2021-03-20 00:03:24 +0530

    Add article show feature

--- db/migrate/20210319065748_create_articles.rb
+++ db/migrate/20210319065748_create_articles.rb
@@ -2,6 +2,7 @@ class CreateArticles < ActiveRecord::Migration[6.1]
   def change
     create_table :articles do |t|
       t.string :title
+      t.text :description
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_065748) do
+ActiveRecord::Schema.define(version: 2021_03_19_134749) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
