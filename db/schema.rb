24cf9086d7508d3c3275965b8f47e663276c673a
Mihir Shri <mihir.shri10@gmail.com>
2021-03-23 00:30:40 +0530

    Create user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_134749) do
+ActiveRecord::Schema.define(version: 2021_03_22_183652) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.text "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
