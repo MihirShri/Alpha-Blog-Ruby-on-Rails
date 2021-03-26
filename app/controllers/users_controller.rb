7ee7fd8ea2a6204bb0a495ce349d07ac4797a495
Mihir Shri <mihir.shri10@gmail.com>
2021-03-26 11:41:47 +0530

    Add edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -11,6 +15,16 @@ class UsersController < ApplicationController
       redirect_to articles_path
     else
       render 'new'
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your profile has been updated successfully!"
+      redirect_to articles_path
+    else
+      render 'edit'
     end
   end
 
