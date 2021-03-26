e5043c19735b794772bf9dc363a6f86a0b1a59f3
Mihir Shri <mihir.shri10@gmail.com>
2021-03-26 11:11:54 +0530

    Add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
   resources :articles
+  resources :users, except: [:new]
 end
