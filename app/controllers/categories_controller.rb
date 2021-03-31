776f83cb0cd415787b5dac3940a5c95154b2617c
Mihir Shri <mihir.shri10@gmail.com>
2021-03-31 23:31:54 +0530

    Added some integration tests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,9 +15,11 @@ class CategoriesController < ApplicationController
   end
 
   def index
+    @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
   def show
+    @category = Category.find(params[:id])
   end
 
   private
