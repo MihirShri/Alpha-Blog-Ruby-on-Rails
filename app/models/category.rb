b2349c8bd5358e58b693bda595efbba9c665df18
Mihir Shri <mihir.shri10@gmail.com>
2021-04-01 12:21:24 +0530

    Add many to many association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25 }
+  has_many :article_categories
+  has_many :articles, through: :article_categories
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_31_055504) do
+ActiveRecord::Schema.define(version: 2021_04_01_045654) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
     t.integer "user_id"
   end
 
